348379b511445feba4edad06c257c1a82d828744
Marius Wichtner <mwichtner@coconut-capital.com>
2017-08-11 21:34:15 +0200

    adding template adding generator

--- generator/generator.py
+++ generator/generator.py
@@ -1,2 +1,13 @@
-def generation():
-    pass
+class Template:
+    def __init__(self, template_uri=None, template_name=None):
+        self.template_uri = template_uri
+        self.template_name = template_name
+
+
+class Generator:
+    def __init__(self, template: Template):
+        self.template = template
+
+    def generate(self):
+        if self.template is not None:
+            return True
--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,5 +1,12 @@
 from unittest import TestCase
 
+from generator.generator import Generator
+
 
 class TestGeneration(TestCase):
     pass
+
+
+class TestGenerator(TestCase):
+    def test_generate(self):
+        generator = Generator()
