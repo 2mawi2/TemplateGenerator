e257f6a502527431bc461f200cda984f87d21a4b
Marius Wichtner <mwichtner@coconut-capital.com>
2017-08-11 23:54:35 +0200

    refactoring

--- generator/Generator.py
+++ generator/Generator.py
@@ -10,10 +10,12 @@ class Generator:
         uri = self.template.template_uri
         name = self.template.template_name
         output = self.template.output_uri
-
         if not FileIO.exists(uri):
             return
+        self.replace_placeholder(name, output, uri)
 
+    @staticmethod
+    def replace_placeholder(name, output, uri):
         content = FileIO.read(uri)
         content = content.replace("###", name)
         FileIO.write(output, content)
