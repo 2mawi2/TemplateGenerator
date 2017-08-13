128c4c77793cf62acc74f151239087ce819d88a8
Marius Wichtner <mwichtner@coconut-capital.com>
2017-08-13 16:21:26 +0200

    adding console output on generation

--- gen/application/run.py
+++ gen/application/run.py
@@ -3,7 +3,7 @@ from gen.application.template_type import TemplateType
 from gen.file_generator.file_generator import FileGenerator
 from gen.utils import values
 
-templateFactory = TemplateFactory(TemplateType.SEARCHABLE, values.default_base_uri)
+templateFactory = TemplateFactory(TemplateType.CRUD, values.default_base_uri)
 templates = templateFactory.create("CRM", "ProductElementPrice")
 
 generator = FileGenerator(templates)
--- gen/file_generator/file_generator.py
+++ gen/file_generator/file_generator.py
@@ -1,25 +1,26 @@
-from gen.file_generator import template
 from gen.file_generator.file_io import FileIO
+from gen.file_generator.template import Template
 
 
 class FileGenerator:
-    def __init__(self, templates: [template]):
+    def __init__(self, templates: [Template]):
         self.templates = templates
 
     def generate(self):
         for t in self.templates:
             self.__validate_template(t)
             self.__create_file_from_template(t)
+            print(f"generated template: {t.template_uri} in directory: {t.output_uri}")
 
     @staticmethod
-    def __validate_template(t: template):
+    def __validate_template(t: Template):
         if not FileIO.exists(t.template_uri):
-            raise FileNotFoundError(t.template_uri)
+            raise FileNotFoundError(f"no such template: {t.template_uri}")
         if FileIO.exists(t.output_uri):
-            raise FileExistsError(t.output_uri)
+            raise FileExistsError(f"template: {t.output_uri} already exsists")
 
     @staticmethod
-    def __create_file_from_template(t: template):
+    def __create_file_from_template(t: Template):
         content = FileIO.read(t.template_uri)
         for key, value in t.replacers.items():
             content = content.replace(key, value)
