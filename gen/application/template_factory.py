be73ac8f6f476b3b299eee312d18eb0d7e3435c1
Marius Wichtner <mwichtner@coconut-capital.com>
2017-08-14 20:39:06 +0200

    refactoring template_factory

--- gen/application/template_factory.py
+++ gen/application/template_factory.py
@@ -12,6 +12,7 @@ class TemplateFactory:
         self.replacers = replacers
         self.base_path = base_path
         self.template_type = template_type
+        self.fac = WaWiUriFactory(self.base_path)
 
     def create(self, package_name, entity_name) -> [Template]:
         if self.replacers is None:
@@ -22,61 +23,28 @@ class TemplateFactory:
             return self.__create_searchable_templates(package_name, entity_name)
 
     def __create_crud_templates(self, package_name, entity_name) -> [Template]:
-        fac = WaWiUriFactory(self.base_path)
-        templates = [
-            Template(values.crud_controller_template,
-                     fac.controller(package_name, entity_name),
-                     self.replacers),
-            Template(values.icrud_controller_template,
-                     fac.i_controller(package_name, entity_name),
-                     self.replacers),
-            Template(values.test_controller_template,
-                     fac.test_controller(package_name, entity_name),
-                     self.replacers),
-            Template(values.crud_repository_template,
-                     fac.repo(package_name, entity_name),
-                     self.replacers),
-            Template(values.icrud_repository_template,
-                     fac.i_repo(package_name, entity_name),
-                     self.replacers),
-            Template(values.test_repository_template,
-                     fac.test_repository(package_name, entity_name),
-                     self.replacers),
-            Template(values.crud_api_controller_template,
-                     fac.api_controller(package_name, entity_name),
-                     self.replacers),
-            Template(values.test_api_controller_template,
-                     fac.test_api_controller(package_name, entity_name),
-                     self.replacers),
-        ]
-        return templates
+        return self.__generate_templates(package_name, entity_name, [
+            (values.crud_controller_template, self.fac.controller),
+            (values.icrud_controller_template, self.fac.i_controller),
+            (values.test_controller_template, self.fac.test_controller),
+            (values.crud_repository_template, self.fac.repo),
+            (values.icrud_repository_template, self.fac.i_repo),
+            (values.test_repository_template, self.fac.test_repository),
+            (values.crud_api_controller_template, self.fac.api_controller),
+            (values.test_api_controller_template, self.fac.test_api_controller),
+        ])
 
     def __create_searchable_templates(self, package_name, entity_name) -> [Template]:
-        fac = WaWiUriFactory(self.base_path)
-        templates = [
-            Template(values.searchable_controller_template,
-                     fac.controller(package_name, entity_name),
-                     self.replacers),
-            Template(values.isearchable_controller_template,
-                     fac.i_controller(package_name, entity_name),
-                     self.replacers),
-            Template(values.test_controller_template,
-                     fac.test_controller(package_name, entity_name),
-                     self.replacers),
-            Template(values.searchable_repository_template,
-                     fac.repo(package_name, entity_name),
-                     self.replacers),
-            Template(values.isearchable_repository_template,
-                     fac.i_repo(package_name, entity_name),
-                     self.replacers),
-            Template(values.test_repository_template,
-                     fac.test_repository(package_name, entity_name),
-                     self.replacers),
-            Template(values.searchable_api_controller_template,
-                     fac.api_controller(package_name, entity_name),
-                     self.replacers),
-            Template(values.test_api_controller_template,
-                     fac.test_api_controller(package_name, entity_name),
-                     self.replacers),
-        ]
-        return templates
+        return self.__generate_templates(package_name, entity_name, [
+            (values.searchable_controller_template, self.fac.controller),
+            (values.isearchable_controller_template, self.fac.i_controller),
+            (values.test_controller_template, self.fac.test_controller),
+            (values.searchable_repository_template, self.fac.repo),
+            (values.isearchable_repository_template, self.fac.i_repo),
+            (values.test_repository_template, self.fac.test_repository),
+            (values.searchable_api_controller_template, self.fac.api_controller),
+            (values.test_api_controller_template, self.fac.test_api_controller),
+        ])
+
+    def __generate_templates(self, package_name: str, entity_name: str, template_output_tuple_list: [(str,)]):
+        return [Template(t, o(package_name, entity_name), self.replacers) for t, o in template_output_tuple_list]
